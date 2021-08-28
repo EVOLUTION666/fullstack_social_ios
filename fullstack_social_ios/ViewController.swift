84130e0c09e0c84bb1d5fe02a3e33901cb8eaa00
Andrey <nefedovandrey1997@gmail.com>
2021-08-28 13:09:14 +0300

    Initial Commit

---
+++ fullstack_social_ios/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  fullstack_social_ios
+//
+//  Created by Andrey on 28.08.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
